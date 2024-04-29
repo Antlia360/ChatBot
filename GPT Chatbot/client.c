65422409ae41ce67c2f1f5d3893f7f70d0dec70f
Aarati Shah <64316366+Antlia360@users.noreply.github.com>
2024-04-29 19:04:19 +0530

    Create client.c

---
+++ GPT Chatbot/client.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#define PORT 5555
+#define SERVER_IP "127.0.0.1"
+
+int main() {
+    int client_socket;
+    struct sockaddr_in server_addr;
+    char message[1024];
+
+    // Create client socket
+    client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (client_socket == -1) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Initialize server address struct
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
+    server_addr.sin_port = htons(PORT);
+
+    // Connect to server
+    if (connect(client_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+        perror("Connection failed");
+        exit(EXIT_FAILURE);
+    }
+    printf("Connected to server\n");
+
+    while (1) {
+        // Get user input
+        printf("user> ");
+        fgets(message, sizeof(message), stdin);
+        // printf("Message : %s", message);
+
+        // Remove trailing newline character
+        message[strcspn(message, "\n")] = '\0';
+
+        // Send message to server
+        send(client_socket, message, strlen(message), 0);
+
+        // Receive and print response from the server
+        int bytes_received = recv(client_socket, message, sizeof(message), 0);
+        if (bytes_received == -1) {
+            perror("Receive failed");
+            exit(EXIT_FAILURE);
+        }
+        message[bytes_received] = '\0';
+        printf("%s\n", message);
+    }
+
+    close(client_socket);
+    return 0;
+}
